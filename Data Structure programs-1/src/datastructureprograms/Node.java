641623ba4147d7fff30642ebbf5be5d233f1c3d5
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-04-07 17:16:28 +0530

    DSP-1

---
+++ Data Structure programs-1/src/datastructureprograms/Node.java
@@ -0,0 +1,11 @@
+package datastructureprograms;
+
+public class Node {
+	String data;
+	Node next;
+	public char[] element;
+	public Node(String data2) {
+		this.data = data2;
+		this.next = null;
+	}
+}
